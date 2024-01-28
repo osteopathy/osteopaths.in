31c65521f4f1d34874e418b4d75002c7f8544b35
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-01-28 10:17:43 +0530

    updated adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { markdoc } from 'svelte-markdoc-preprocess';
 import { dirname, join } from 'path';

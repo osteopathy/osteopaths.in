ab27b26c550de9735d6f13c30f25ae9725e81a98
q1b <sukhpreetben10@gmail.com>
2025-02-28 14:01:52 +0530

    favicon update

---
+++ src/routes/(api)/api/v1/appointment/helpers.ts
@@ -0,0 +1,45 @@
+import type {
+	ServiceAppointment,
+	InsertServiceAppointment
+} from "$lib/database/schema/service/appointment";
+
+export const serviceAppointment = {
+	getAll: async () => {
+		const res = await fetch(`/api/v1/appointment`);
+		return res.json() as Promise<{
+			data: ServiceAppointment[];
+		}>;
+	},
+	get: async (id: string) => {
+		const res = await fetch(`/api/v1/appointment?id=${id}`);
+		return res.json() as Promise<{
+			data: ServiceAppointment;
+		}>;
+	},
+	new: async (values: InsertServiceAppointment) => {
+		const res = await fetch("/api/v1/appointment/", {
+			method: "POST",
+			body: JSON.stringify(values)
+		});
+		return res.json() as Promise<{
+			data: ServiceAppointment;
+		}>;
+	},
+	put: async (id: string, values: Partial<Omit<ServiceAppointment, "id">>) => {
+		const res = await fetch(`/api/v1/appointment?id=${id}`, {
+			method: "PUT",
+			body: JSON.stringify(values)
+		});
+		return res.json() as Promise<{
+			data: ServiceAppointment;
+		}>;
+	},
+	del: async (id: number) => {
+		const res = await fetch(`/api/v1/appointment?id=${id}`, {
+			method: "DELETE"
+		});
+		return res.json() as Promise<{
+			data: ServiceAppointment;
+		}>;
+	}
+};

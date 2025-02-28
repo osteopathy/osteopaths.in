c6de16d8cdd947fbb61de59604548ef2d933f067
q1b <sukhpreetben10@gmail.com>
2025-02-28 15:30:57 +0530

    use dynamic env variables

--- src/lib/server/auth/google.ts
+++ src/lib/server/auth/google.ts
@@ -1,11 +1,9 @@
 import { Google } from "arctic";
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { dev } from "$app/environment";
 
 export const google = new Google(
-	GOOGLE_CLIENT_ID,
-	GOOGLE_CLIENT_SECRET,
-	dev
-		? "http://localhost:5173/login/google/callback"
-		: "https://osteopaths.in/login/google/callback"
+	env.GOOGLE_CLIENT_ID,
+	env.GOOGLE_CLIENT_SECRET,
+	dev ? "http://localhost:5173/login/google/callback" : env.GOOGLE_REDIRECT_URI
 );

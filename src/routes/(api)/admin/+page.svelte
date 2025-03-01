2cbc5cf443f28351238cf9ecad40674fd16738c0
q1b <sukhpreetben10@gmail.com>
2025-03-01 19:17:58 +0530

    Simple Impersonation Implementation

--- src/routes/(api)/admin/+page.svelte
+++ src/routes/(api)/admin/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
 	import Avatar from "$lib/components/ui/avatar/avatar.svelte";
 	import AppShell from "../../AppShell.svelte";
 	import type { PageProps } from "./$types";
@@ -14,6 +15,10 @@
 		{#each data.users as user}
 			<li class="mb-10 flex flex-wrap items-center gap-4">
 				<Avatar class="size-12" src={user.picture} alt="Pic" fallback="FA" />
+				<form method="post" use:enhance>
+					<input type="text" id="user_id" name="user_id" value={user.id} />
+					<button>Sign In as</button>
+				</form>
 				<span>{user.name}</span>
 				<span>{user.role}</span>
 				<span>{user.email}</span>

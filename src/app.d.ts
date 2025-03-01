74631422d53028b8753d6ebfec83416aac6f1001
q1b <sukhpreetben10@gmail.com>
2025-03-01 08:40:48 +0530

    Hybrid JWT Implemention

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,7 +10,6 @@ declare global {
 	namespace App {
 		interface Locals {
 			user: import("$lib/server/auth/session").SessionValidationResult["user"];
-			session: import("$lib/server/auth/session").SessionValidationResult["session"];
 		}
 	}
 }

f25a93c9f769d57783068818cd37f602dfe3e7a3
q1b <sukhpreetben10@gmail.com>
2025-03-03 15:37:41 +0530

    theme: [dark | light | system]

--- src/lib/components/select-theme.svelte
+++ src/lib/components/select-theme.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <select
-	class="bg-layer-3 hover:bg-layer-3 border-layer-6 hover:border-layer-7 rounded-md border px-2 py-1 shadow-sm"
+	class="bg-layer-3 hover:bg-layer-3 border-layer-6 hover:border-layer-7 rounded-md border py-1 pl-2 shadow-sm"
 	onchange={(event) => {
 		const selectedMode = event.currentTarget.value as "light" | "dark" | "system";
 		setMode(selectedMode);
 	}}
 	value={$mode}
 >
-	<option value="light">Light</option>
-	<option value="dark">Dark</option>
-	<option value="system">System</option>
+	<option value="light">Theme: Light</option>
+	<option value="dark">Theme: Dark</option>
+	<option value="system">Theme: System</option>
 </select>

65981ae56030875648d61f058b468cb37f0ded87
q1b <sukhpreetben10@gmail.com>
2025-03-04 09:43:47 +0530

    update navigation path

--- src/routes/services/[service]/+page.svelte
+++ src/routes/services/[service]/+page.svelte
@@ -8,7 +8,7 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<AppShell back="/app" heading="Osteopaths" class="mt-4">
+<AppShell back="/app/service" heading="Osteopaths" class="mt-4">
 	<div class="flex w-full items-start gap-x-3 px-2.5 sm:gap-x-6">
 		{#each data.serviceProviderList as serviceProvider}
 			<a
